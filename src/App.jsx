0d80395fd8ba8d7c624bb8224c9cd110b8e9b16d
Alexander Lind <alex.anonym@gmail.com>
2023-02-14 08:56:36 +0100

    add final touches to the exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
 import Logo from "./assets/logo.svg";
 import "./App.css";
+import data from "./assets/data.json";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const sum = data.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+  let maxAmount = 0;
+  data.forEach((item) => {
+    if (item.amount > maxAmount) {
+      maxAmount = item.amount;
+    }
+  });
+  data.forEach((item) => {
+    item.height = Math.floor((item.amount / maxAmount) * 120) + "px";
+  });
+  console.log(sum);
 
   return (
     <div className="App">
@@ -18,16 +31,31 @@ function App() {
       </div>
       <div className="lower-card">
         <h1>Spending - Last 7 days</h1>
-        <div className="bar-chart"> BAR CHART HERE</div>
+        <div className="bar-chart">
+          {data.map((item) => {
+            return (
+              <div className="bar">
+                <div className="bar-money-spend">
+                  <p className="amount">${item.amount}</p>
+                  <div
+                    className="bar-box"
+                    style={{ height: item.height }}
+                  ></div>
+                  <p className="day">{item.day}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
         <hr />
         <div className="lower-card-bottom">
           <div className="left">
-            <h3>Total this month</h3>
-            <h1>Sum</h1>
+            <h3>Total this week</h3>
+            <h1>${sum}</h1>
           </div>
           <div className="right">
-            <p>Percent gain</p>
-            <h3>from last month</h3>
+            <h4>+2.4%</h4>
+            <h3>from last week</h3>
           </div>
         </div>
       </div>
